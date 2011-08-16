86066958ca509c3373d0ff17c2eeeb2ada4cf0ff
misho <misho@e7aaec5d-71a6-447d-891b-76dfd174eba1>
2011-08-16 06:33:23 +0000

    FeynRules: SM.frのミスを修正したり，UFO/FAパッケージを作ったり
    git-svn-id: https://dav.misho-web.com/svn/tmp/trunk@31 e7aaec5d-71a6-447d-891b-76dfd174eba1

---
+++ old/FeynRules-1.6/Models/SM/mySM.m
@@ -0,0 +1,15 @@
+(* ::Package:: *)
+
+(* ::Input:: *)
+(*SetDirectory[ToFileName[{$LibDirectory,"Models","SM"}]]*)
+(*<<FeynRules`;*)
+(*LoadModel["SM.fr"];*)
+(*WriteUFO[LGauge,LHiggs,LFermions,LYukawa,LGhost];*)
+
+
+(* ::Input:: *)
+(*SetDirectory[ToFileName[{$LibDirectory,"Models","SM"}]]*)
+(*<<FeynRules`;*)
+(*LoadModel["SM.fr"];*)
+(*FeynmanGauge=True;*)
+(*WriteFeynArtsOutput[LGauge,LHiggs,LFermions,LYukawa,LGhost];*)
