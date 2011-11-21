fbb727ec09d26f6fa095009f5d39fdfc905e7da3
misho <misho@e7aaec5d-71a6-447d-891b-76dfd174eba1>
2011-11-21 13:46:48 +0000

    SM FeynRules solve m file を改良
    git-svn-id: https://dav.misho-web.com/svn/tmp/trunk@44 e7aaec5d-71a6-447d-891b-76dfd174eba1

--- old/FeynRules-1.6/Models/SM/mySM.m
+++ old/FeynRules-1.6/Models/SM/mySM.m
@@ -1,15 +1,27 @@
 (* ::Package:: *)
 
-(* ::Input:: *)
-(*SetDirectory[ToFileName[{$LibDirectory,"Models","SM"}]]*)
-(*<<FeynRules`;*)
-(*LoadModel["SM.fr"];*)
-(*WriteUFO[LGauge,LHiggs,LFermions,LYukawa,LGhost];*)
+SetDirectory[ToFileName[{$FeynModelsDirectory,"SM"}]]
+<<FeynRules`;
+LoadModel["SM.fr"];
+FeynmanGauge=True;
+(*LoadRestriction["Cabibbo.rst","Massless.rst"]*)
+WriteUFO[LGauge,LHiggs,LFermions,LYukawa,LGhost];
 
 
-(* ::Input:: *)
-(*SetDirectory[ToFileName[{$LibDirectory,"Models","SM"}]]*)
-(*<<FeynRules`;*)
-(*LoadModel["SM.fr"];*)
-(*FeynmanGauge=True;*)
-(*WriteFeynArtsOutput[LGauge,LHiggs,LFermions,LYukawa,LGhost];*)
+SetDirectory[ToFileName[{$FeynModelsDirectory,"SM"}]]
+<<FeynRules`;
+LoadModel["SM.fr"];
+FeynmanGauge=True;
+(*LoadRestriction["Cabibbo.rst","Massless.rst"]*)
+WriteFeynArtsOutput[LGauge,LHiggs,LFermions,LYukawa,LGhost];
+
+
+CheckHermiticity[LSM,FlavorExpand->True]
+CheckMassSpectrum[LSM]
+CheckKineticTermNormalisation[LSM,FlavorExpand->True]
+CheckDiagonalKineticTerms[LSM]
+CheckDiagonalMassTerms[LSM]
+CheckDiagonalQuadraticTerms[LSM]
+
+
+(* Feynman or Unitary :: FA>T/F SH>F MG4>F CH>T WH>T *)
