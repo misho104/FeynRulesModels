4dba0683c85dc97570c081f1e01438124b7559b5
misho <misho@e7aaec5d-71a6-447d-891b-76dfd174eba1>
2011-11-21 15:20:37 +0000

    FeynRules MSSM 1.3.8 へ update; 生成ファイル修正; FeynArtsまで作成; UFOは未更新
    git-svn-id: https://dav.misho-web.com/svn/tmp/trunk@45 e7aaec5d-71a6-447d-891b-76dfd174eba1

--- old/models/MSSM/MSSM.m
+++ old/models/MSSM/MSSM.m
@@ -17,18 +17,25 @@ LagFeynman=lagr/.{ghG[__]->0, ghGbar[__]->0,ghWp->0,ghWpbar->0,ghWmbar->0,ghWm->
 {Definition[lagr],Definition[LagFeynman]}>>MSSM.dat
 
 
-SetDirectory[NotebookDirectory[]]
-<<FeynRules`
-LoadModel["MSSM.fr"];
-<<MSSM.dat;
-WriteUFO[LagFeynman, Exclude4Scalars->False];
+(*sps1a.dat    for CalcHEP; not for Whizard*)
+(*sps1a_wo.dat for Whizard; not for CalcHEP*)
 
 
 SetDirectory[NotebookDirectory[]]
 <<FeynRules`
 LoadModel["MSSM.fr"];
 <<MSSM.dat;
-WriteFeynArtsOutput[LagFeynman];
+ReadLHAFile[Input->"sps1a.dat"];
+\[AliasDelimiter]WriteUFO[LagFeynman, Exclude4Scalars->False];
+
+
+SetDirectory[NotebookDirectory[]]
+<<FeynRules`
+LoadModel["MSSM.fr"];
+<<MSSM.dat;
+ReadLHAFile[Input->"sps1a.dat"];
+WriteRestrictionFile[];LoadRestriction["ZeroValues.rst"];DeleteFile["ZeroValues.rst"];
+WriteFeynArtsOutput[LagFeynman, Exclude4Scalars->False];
 
 
 
--- old/models/MSSM/orig/my_mssm.m
+++ old/models/MSSM/orig/my_mssm.m
@@ -16,11 +16,17 @@ lagr=Lag
 Definition[lagr]>>lagrangian.dat
 
 
+(*sps1a.dat    for CalcHEP; not for Whizard*)
+(*sps1a_wo.dat for Whizard; not for CalcHEP*)
+
+
 SetDirectory[NotebookDirectory[]]
 <<FeynRules`
 LoadModel["mssm.fr"];
 <<lagrangian.dat;
 LagFeynman=lagr/.{ghG[__]->0, ghGbar[__]->0,ghWp->0,ghWpbar->0,ghWmbar->0,ghWm->0,ghZ->0,ghZbar->0,ghA->0,ghAbar->0, G0->0,GP->0,GPbar->0};
+ReadLHAFile[Input->"sps1a.dat"];
+WriteRestrictionFile[]; LoadRestriction["ZeroValues.rst"]; DeleteFile["ZeroValues.rst"];
 WriteUFO[LagFeynman, Exclude4Scalars->False];
 
 
@@ -29,4 +35,9 @@ SetDirectory[NotebookDirectory[]]
 LoadModel["mssm.fr"];
 <<lagrangian.dat;
 LagFeynman=lagr/.{ghG[__]->0, ghGbar[__]->0,ghWp->0,ghWpbar->0,ghWmbar->0,ghWm->0,ghZ->0,ghZbar->0,ghA->0,ghAbar->0, G0->0,GP->0,GPbar->0};
+ReadLHAFile[Input->"sps1a.dat"];
+WriteRestrictionFile[]; LoadRestriction["ZeroValues.rst"]; DeleteFile["ZeroValues.rst"];
 WriteFeynArtsOutput[LagFeynman];
+
+
+
