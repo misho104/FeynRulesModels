2ae291d54dd062f9f823de373ff3175924500c36
misho <misho@e7aaec5d-71a6-447d-891b-76dfd174eba1>
2013-09-25 04:26:52 +0000

    MSSM modelfile (same version) adopt to FR 1.6.18
    git-svn-id: https://dav.misho-web.com/svn/tmp/trunk@70 e7aaec5d-71a6-447d-891b-76dfd174eba1

--- old/models/MSSM/MSSM.sh
+++ old/models/MSSM/MSSM.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-(*sps1a.dat    for CalcHEP; not for Whizard*)
-(*sps1a_wo.dat for Whizard; not for CalcHEP*)
+# sps1a.dat    for CalcHEP; not for Whizard
+# sps1a_wo.dat for Whizard; not for CalcHEP
 
 MATH=MathKernel
 
 # ------ Lagrangian
 rm MSSM.dat
-$MATH << _EOC_
+$MATH << '_EOC_'
 SetDirectory[NotebookDirectory[]]
 <<FeynRules`
 LoadModel["MSSM.fr"];
@@ -21,7 +21,7 @@ LagNoGhNG=lagr/.{ghG[__]->0, ghGbar[__]->0,ghWp->0,ghWpbar->0,ghWmbar->0,ghWm->0
 {Definition[lagr],Definition[LagNoGhNG]}>>MSSM.dat
 _EOC_
 
-$MATH <<_EOC_
+$MATH <<'_EOC_'
 SetDirectory[NotebookDirectory[]]
 <<FeynRules`
 LoadModel["MSSM.fr"];
@@ -33,7 +33,7 @@ _EOC_
 rm -rf MSSM_sps1a_UFO
 mv MSSM_UFO MSSM_sps1a_UFO
 
-$MATH <<_EOC_
+$MATH <<'_EOC_'
 SetDirectory[NotebookDirectory[]]
 <<FeynRules`
 LoadModel["MSSM.fr"];
@@ -45,7 +45,7 @@ _EOC_
 rm -rf MSSM_sps1a_FA
 mv MSSM_FA MSSM_sps1a_FA
 
-$MATH <<_EOC_
+$MATH <<'_EOC_'
 SetDirectory[NotebookDirectory[]]
 <<FeynRules`
 LoadModel["MSSM.fr"];
@@ -57,7 +57,7 @@ _EOC_
 rm -rf MSSM_generic_UFO
 mv MSSM_UFO MSSM_generic_UFO
 
-$MATH << _EOC_
+$MATH << '_EOC_'
 SetDirectory[NotebookDirectory[]]
 <<FeynRules`
 LoadModel["MSSM.fr"];
